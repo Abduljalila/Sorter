4022e1f0988b4d48f1571c6f521776d937fa02b3
Abduljalila <temsa7aa@gmail.com>
2024-02-14 14:59:48 +0300

    editing name of  selctionSort

--- src/Sorter.java
+++ src/Sorter.java
@@ -6,22 +6,21 @@ public class Sorter {
         int[] array = {5, 7, 3, 1, 4};
         System.out.println("Original array: " + Arrays.toString(array));
 
-        recursive(array, 0);
+        selctionSort(array, 0);
 
         System.out.println("Sorted array: " + Arrays.toString(array));
     }
-    public static void recursive(int[] list, int startIndex) {
+    public static void selctionSort(int[] list, int startIndex) {
         if (startIndex < list.length - 1) {
             int minIndex = findMinIndex(list, startIndex);
             int temp = list[minIndex];
             list[minIndex] = list[startIndex];
             list[startIndex] = temp;
-            recursive(list, startIndex + 1);
+            selctionSort(list, startIndex + 1);
         }
     }
     private static int findMinIndex(int[] arr, int startIndex) {
         int minIndex = startIndex;
-
         for (int i = startIndex + 1; i < arr.length; i++) {
             if (arr[i] < arr[minIndex]) {
                 minIndex = i;
