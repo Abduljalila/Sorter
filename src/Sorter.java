e16eddcaf407d1dc9dc90ddfd133df805f947fb5
TKScorpion <Theycallmemohammed@gmail.com>
2024-02-15 15:52:42 +0300

    Update Sorter.java

--- src/Sorter.java
+++ src/Sorter.java
@@ -9,6 +9,12 @@ public class Sorter {
         selectionSort(array, 0);
 
         System.out.println("Sorted array: " + Arrays.toString(array));
+
+        insertionSort(array);
+        System.out.println("Sorted array using insertionSort:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
     }
     public static void selectionSort(int[] list, int startIndex) {
         if (startIndex < list.length - 1) {
@@ -28,5 +34,19 @@ public class Sorter {
         }
 
         return minIndex;
+    }
+    public static void insertionSort (int[] arr){
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
 }
